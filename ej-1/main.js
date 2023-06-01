e909995cb64a5f3d1ea3711d408821a65f9d8c6d
anderconsu <anderconsu@gmail.com>
2023-06-01 10:51:04 +0200

    Finished

--- ej-1/main.js
+++ ej-1/main.js
@@ -1,1 +1,17 @@
-console.log("hello")
+// Variables a elegir//
+let palabra = 'Abecedario'
+let seleccionLetra = 'a'
+
+// Funcion para contar las letras en la palabra
+const chcount = (palabra, selec) => {
+    let counter = 0;
+    for (const letra of palabra) 
+    {
+     if(selec == letra.toLowerCase()){
+        counter++
+     }
+    }
+    return counter;
+}
+// Muestra en consola
+console.log(chcount(palabra, seleccionLetra))
--- ej-2/main.js
+++ ej-2/main.js
@@ -1,1 +1,34 @@
-console.log("hello")
+// Pedir numero
+const pedirn = () => {
+  let numero = prompt("¿Qué número desea elegir?");
+  while (isNaN(numero) || numero == 0 || numero == '' || numero == undefined) {
+    numero = prompt("No has ingresado un número o has ingresado 0, por favor, escribe un numero valido");
+  }
+  return parseInt(numero);
+};
+
+// Obtener numeros impares entre numero elegido + 50
+const nsimpar = (numero) => {
+  const limit = numero + 50;
+  listImpar = [];
+  for (let num = numero; num <= limit; num++) {
+    if (num % 2 != 0) {
+      listImpar.push(num);
+    }
+  }
+  return listImpar;
+};
+
+// Imprimir por pantalla array
+const imprimirArray = (lista) => {
+  const body = document.getElementsByTagName("body");
+  const p = document.createElement("p");
+  p.innerText = lista;
+  document.body.append(p);
+};
+
+// Ejecucion
+let numero = pedirn();
+let numerosimpares = nsimpar(numero);
+imprimirArray(numerosimpares);
+
